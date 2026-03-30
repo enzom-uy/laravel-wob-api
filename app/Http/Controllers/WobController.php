2f7a446e3dba67cdbc81141fc526dac1a9e69d87
enzom-uy <enzom.dev@gmail.com>
2026-03-30 18:33:40 -0300

    validation, random wob api and enforce json response middleware

--- app/Http/Controllers/WobController.php
+++ app/Http/Controllers/WobController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Wob;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Symfony\Component\Console\Input\Input;
 
 class WobController extends Controller
 {
@@ -16,9 +19,14 @@ class WobController extends Controller
 
     public function search(Request $request)
     {
+        $validate = $request->validate([
+            'query' => 'required|max:255',
+            'page' => 'required|numeric|integer',
+            'perPage' => 'required|numeric|integer|max:25'
+        ]);
         $query = $request->input('query');
-        $page = $request->input('page', 1);
-        $perPage = 10;
+        $page = (int)$request->input('page', 1);
+        $perPage = (int)$request->input('perPage', 10);
 
         $wobsText = Wob::where('text', 'like', '%' . $query . '%')->get();
         $wobsTag = Wob::whereHas('tags', function ($q) use ($query) {
@@ -32,11 +40,36 @@ class WobController extends Controller
 
 
         $response = [
-            'data' => $results,
             'currentPage' => $page,
             'perPage' => $perPage,
             'total' => $total,
-            'lastPage' => ceil($total / $perPage)
+            'lastPage' => ceil($total / $perPage),
+            'data' => $results
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    public function random(Request $request) {
+       $request->validate([
+            'afterDate' => ['nullable', 'date', 'date_format:d-m-Y', 'before:today']],
+            [
+                'afterDate.before' => 'Date must be before ' . today()->format('d-m-Y') . '.',
+                'afterDate.date_format' => 'Formate date must be DD-MM-YYYY.'
+            ]
+       );
+
+        $afterDate = $request->input('afterDate');
+
+        $afterDateForQuery = $afterDate ? \Carbon\Carbon::createFromFormat('d-m-Y', $afterDate)->format('Y-m-d') : null;
+
+        $randomWob = Wob::when($afterDateForQuery, fn($q) =>
+            $q->where('date', '>', $afterDateForQuery)
+        )->inRandomOrder()->first();
+        logger($randomWob);
+        $response = [
+            'data' => $randomWob,
+            'after' => $afterDate
         ];
 
         return response()->json($response, 200);
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,8 @@
 <?php
 
 use App\Http\Controllers\WobController;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return 'Hello world';
-});
-
-
 Route::get('/wob', [WobController::class, 'test']);
-Route::post('/wob/search', [WobController::class, 'search']);
+Route::get('/wob/search', [WobController::class, 'search']);
+Route::get('/wob/random', [WobController::class, 'random']);
